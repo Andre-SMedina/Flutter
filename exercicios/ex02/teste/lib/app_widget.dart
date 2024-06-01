f33649ee03a2622ab325b489779b3a5898172dd8
andre <andre-smedina>
2024-06-01 10:01:32 -0300

    final

---
+++ exercicios/ex02/teste/lib/app_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
