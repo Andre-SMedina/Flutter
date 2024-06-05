9c8186072f031d276484e835d1a930463be11cbd
andre <andre-smedina>
2024-06-04 21:24:28 -0300

    add

--- exercicios/ex03/inicio3/lib/_comum/cores.dart
+++ exercicios/ex03/inicio3/lib/_comum/cores.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class MyColors {
   static const Color azul = Color(0xff4f52ff);
   static const Color branco = Color(0xffdbffff);
+  static const Color botao = Color(0xff2b52b5);
 }
--- exercicios/ex03/inicio3/lib/screens/login_screen.dart
+++ exercicios/ex03/inicio3/lib/screens/login_screen.dart
@@ -9,6 +9,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool? continueConnected = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,35 +19,102 @@ class _LoginScreenState extends State<LoginScreen> {
         title: const Text(style: TextStyle(color: MyColors.branco), 'My App'),
       ),
       body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 50),
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
         decoration: const BoxDecoration(
             gradient: LinearGradient(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 colors: [MyColors.azul, MyColors.branco])),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Image(
-              image: AssetImage('assets/images/halter.png'),
-              height: 125,
-            ),
-            const Padding(padding: EdgeInsets.only(bottom: 15)),
-            const Text(
-              'Entrar',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: MyColors.branco,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        //serve para dar scroll na tela
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Image(
+                image: AssetImage('assets/images/halter.png'),
+                height: 125,
               ),
-            ),
-            Form(
-                child: Column(
-              children: [TextFormField.new()],
-            ))
-          ],
+              const Padding(padding: EdgeInsets.only(bottom: 15)),
+              const Text(
+                'Entrar',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: MyColors.branco,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    style: const TextStyle(color: MyColors.branco),
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: MyColors.branco)),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: MyColors.branco)),
+                        labelText: 'E-mail',
+                        labelStyle: TextStyle(color: MyColors.branco),
+                        prefixIcon: Icon(
+                          Icons.mail_outline,
+                          color: MyColors.branco,
+                        )),
+                  ),
+                  TextFormField(
+                    style: const TextStyle(color: MyColors.branco),
+                    decoration: const InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: MyColors.branco)),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: MyColors.branco)),
+                        labelText: 'Senha',
+                        labelStyle: TextStyle(color: MyColors.branco),
+                        prefixIcon: Icon(
+                          Icons.key_outlined,
+                          color: MyColors.branco,
+                        )),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print('Funcionou!');
+                    },
+                    child: const Text(
+                        style: TextStyle(color: MyColors.branco),
+                        'Esqueceu a senha?'),
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                          value: continueConnected,
+                          onChanged: (newValue) {
+                            setState(() {
+                              continueConnected = newValue;
+                            });
+                          }),
+                      const Text(
+                          style: TextStyle(color: MyColors.branco),
+                          'Continuar conectado?')
+                    ],
+                  )
+                ],
+              )),
+              ElevatedButton(
+                style:
+                    ElevatedButton.styleFrom(backgroundColor: MyColors.botao),
+                onPressed: () {},
+                child: const Text(
+                    style: TextStyle(color: MyColors.branco, fontSize: 18),
+                    'Login'),
+              )
+            ],
+          ),
         ),
       ),
     );
