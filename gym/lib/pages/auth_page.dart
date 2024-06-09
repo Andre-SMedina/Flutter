2937c1ac7c9935ce655df5754ab00c9e7042c946
andre <andre-smedina>
2024-06-08 23:00:37 -0300

    add

--- gym/lib/pages/auth_page.dart
+++ gym/lib/pages/auth_page.dart
@@ -5,8 +5,24 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.blue,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(
+            "assets/images/halter.png",
+            height: 128,
+          ),
+          const Text(
+            textAlign: TextAlign.center,
+            'GymApp',
+            style: TextStyle(
+                fontSize: 48, color: Colors.white, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
     );
   }
 }
