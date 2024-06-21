c2a50fa94e659cda9fd357117716ed3af180ec73
andre-smedina <andrelseu@gmail.com>
2024-06-21 07:13:39 -0300

    valid

--- alura/lib/apps/app1/tarefas/form_screen.dart
+++ alura/lib/apps/app1/tarefas/form_screen.dart
@@ -12,6 +12,7 @@ class _FormScreenState extends State<FormScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController ageController = TextEditingController();
   TextEditingController imageController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -34,37 +35,45 @@ class _FormScreenState extends State<FormScreen> {
             width: 375,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  textFields('Nome', nameController),
-                  textFields('Idade', ageController, type: true),
-                  textFields('Imagem', imageController),
-                  Container(
-                    height: 100,
-                    width: 72,
-                    decoration: BoxDecoration(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    textFields('Nome', nameController),
+                    textFields('Idade', ageController,
+                        type: true, valid: 'idade'),
+                    textFields('Imagem', imageController, valid: 'image'),
+                    Container(
+                      height: 100,
+                      width: 72,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                          border: Border.all(width: 2)),
+                      child: ClipRRect(
                         borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                        border: Border.all(width: 2)),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: Image.network(
-                        imageController.text,
-                        fit: BoxFit.cover,
-                        errorBuilder: (BuildContext context, Object exception,
-                            StackTrace? stackTrace) {
-                          return Image.asset('assets/images/noImage.png');
-                        },
+                        child: Image.network(
+                          imageController.text,
+                          fit: BoxFit.cover,
+                          errorBuilder: (BuildContext context, Object exception,
+                              StackTrace? stackTrace) {
+                            return Image.asset('assets/images/noImage.png');
+                          },
+                        ),
                       ),
                     ),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {});
-                      },
-                      child: Text('Adicionar')),
-                ],
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_formKey.currentState!.validate()) {
+                              print('Sucesso!');
+                            }
+                          });
+                        },
+                        child: Text('Adicionar')),
+                  ],
+                ),
               ),
             ),
           ),
@@ -75,8 +84,34 @@ class _FormScreenState extends State<FormScreen> {
 
   TextFormField textFields(
       String textField, TextEditingController inputController,
-      {bool type = false}) {
+      {bool type = false, String valid = 'text'}) {
     return TextFormField(
+      validator: (value) {
+        if (valid == 'text') {
+          if (value != null && value.isEmpty) {
+            return 'Insira o nome da tarefa';
+          }
+        }
+        if (valid == 'idade') {
+          if (value!.isEmpty ||
+              int.parse(value) > 100 ||
+              int.parse(value) < 5) {
+            return 'Idade deve ser entre 5 e 100';
+          }
+        }
+        if (valid == 'image') {
+          if (value!.isEmpty) {
+            return 'Insira uma URL de imagem';
+          }
+        }
+        return null;
+      },
+      //TODO: Definir a entrada somente de números
+      inputFormatters: [
+        (valid == 'idade')
+            ? FilteringTextInputFormatter.digitsOnly
+            : FilteringTextInputFormatter.singleLineFormatter,
+      ],
       keyboardType: (type) ? TextInputType.number : TextInputType.text,
       controller: inputController,
       onChanged: (v) {
