82e6fefe60ffd80c799ad4247ec1519963d93c32
andre-smedina <andrelseu@gmail.com>
2024-07-02 23:08:44 -0300

    app ok (falta persistencia de dados)

--- mytime/lib/main.dart
+++ mytime/lib/main.dart
@@ -30,13 +30,10 @@ class _MyTimeState extends State<MyTime> {
     return Future.value(registers);
   }
 
-  Future<List<Widget>> getHistoric() {
-    return Future.value(historic);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 190, 13, 0),
@@ -55,14 +52,13 @@ class _MyTimeState extends State<MyTime> {
                 ElevatedButton(
                   onPressed: () {
                     setState(() {
-                      // if (timeTotalGeral.day != DateTime.now().day) {
-                      if (true) {
-                        historic.add(historicCard());
-                        // registers = [];
-                        // timeTotalTxt = '00:00:00';
-                        // oldTimeTxt = '';
-                        // timeTotalGeral = DateTime(DateTime.now().year,
-                        //     DateTime.now().month, DateTime.now().day, 0, 0);
+                      if (timeTotalGeral.day != DateTime.now().day) {
+                        historicCard();
+                        registers = [];
+                        timeTotalTxt = '00:00:00';
+                        oldTimeTxt = '';
+                        timeTotalGeral = DateTime(DateTime.now().year,
+                            DateTime.now().month, DateTime.now().day, 0, 0);
                       }
 
                       List<dynamic> result = dayRegisterList(
@@ -121,29 +117,42 @@ class _MyTimeState extends State<MyTime> {
                             });
                       }),
                 ),
-                Text(
-                  'Histórico',
-                  style: MyTextStyles.title,
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 7.0),
+                  child: Text(
+                    'Histórico',
+                    style: MyTextStyles.title,
+                  ),
                 ),
-                Container(
+                Ink(
                   height: 200,
                   width: 340,
-                  margin: const EdgeInsets.only(top: 5),
+                  // margin: const EdgeInsets.only(top: 5),
                   decoration: BoxDecoration(
                       color: Colors.grey[600],
                       borderRadius: BorderRadius.circular(10)),
                   padding:
-                      const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-                  child: FutureBuilder(
-                      future: getHistoric(),
-                      builder: (_, data) {
-                        return ListView.builder(
-                            itemCount: historic.length,
-                            itemBuilder: (_, int index) {
-                              return historic[index];
-                            });
-                      }),
-                ),
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  child: CustomScrollView(
+                    slivers: <Widget>[
+                      SliverGrid(
+                        delegate: SliverChildBuilderDelegate((context, index) {
+                          return historic[index];
+                        }, childCount: historic.length),
+                        //configura para 2 colunas
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          //espaço entre os cards
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          //tamanho de cada cartão, onde o primeiro valor é a largura e o segundo a altura 158/194
+                          childAspectRatio: 3.5,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
@@ -151,8 +160,32 @@ class _MyTimeState extends State<MyTime> {
       ),
     );
   }
-}
 
-Widget historicCard() {
-  return Text('Blz');
+  historicCard() {
+    String day = formatTime(timeTotalGeral.day.toString());
+    String month = formatTime(timeTotalGeral.month.toString());
+    String year = formatTime(timeTotalGeral.year.toString());
+    String hour = formatTime(timeTotalGeral.hour.toString());
+    String minute = formatTime(timeTotalGeral.minute.toString());
+    String second = formatTime(timeTotalGeral.second.toString());
+    historic.add(Container(
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        children: [
+          Text(
+            '$day/$month/$year',
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '$hour:$minute:$second',
+          ),
+        ],
+      ),
+    ));
+  }
+
+  formatTime(String time) {
+    return (time.length == 1) ? '0$time' : time;
+  }
 }
